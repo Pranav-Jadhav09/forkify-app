31039c60afe4026c278f5407747ac308ac7c9155
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-02 15:43:59 +0530

    feat: ✨🚀 added eventlistners

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,11 +26,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // 1) Loading Recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13`
-      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -153,4 +156,4 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
