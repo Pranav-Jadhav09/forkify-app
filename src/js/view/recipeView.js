06e45ad70c0b1bff462a9e2e72c6f844e737dc64
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-03 11:10:32 +0530

    refact: 🔄 ingredients quantity code

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -87,23 +87,7 @@ class RecipeView {
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
 
-    ${this.#data.ingredients
-      .map((ing) => {
-        return `<li class="recipe__ingredient">
-      <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
-      </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ""
-      }</div>
-      <div class="recipe__description">
-        <span class="recipe__unit">${ing.unit}</span>
-        ${ing.description}
-      </div>
-    </li>
-`;
-      })
-      .join("")}
+    ${this.#data.ingredients.map(this.#generateMarkupIngredient).join("")}
     </ul>
   </div>
 
@@ -126,6 +110,22 @@ class RecipeView {
     </a>
   </div>
     `;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `<li class="recipe__ingredient">
+  <svg class="recipe__icon">
+    <use href="${icons}#icon-check"></use>
+  </svg>
+  <div class="recipe__quantity">${
+    ing.quantity ? new Fraction(ing.quantity).toString() : ""
+  }</div>
+  <div class="recipe__description">
+    <span class="recipe__unit">${ing.unit}</span>
+    ${ing.description}
+  </div>
+</li>
+`;
   }
 }
 
