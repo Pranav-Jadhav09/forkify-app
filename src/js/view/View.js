4a48ae0a1644297f3e81281f823920846b4d5cc8
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-03 15:00:58 +0530

    refact: 🔄 created parent View class

--- src/js/view/View.js
+++ src/js/view/View.js
@@ -1,1 +1,56 @@
-export default class View {}
+import icons from "url:../../assets/icons.svg";
+
+export default class View {
+  _data;
+
+  render(data) {
+    this._data = data;
+    const markup = this._generaterMarkup();
+    this._clear();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  _clear() {
+    this._parentEl.innerHTML = "";
+  }
+
+  renderSpinner = function () {
+    const markup = `<div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+   </div>`;
+    this._clear();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  };
+
+  renderError(message = this._errorMessage) {
+    const markup = ` 
+    <div class="error">
+    <div>
+      <svg>
+        <use href="${icons}#icon-alert-triangle"></use>
+      </svg>
+    </div>
+    <p>${message}</p>
+  </div>`;
+
+    this._clear();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `  
+    <div class="message">
+    <div>
+      <svg>
+        <use href="${icons}#icon-smile"></use>
+      </svg>
+    </div>
+    <p>${message}</p>
+  </div> `;
+
+    this._clear();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  }
+}
--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -2,62 +2,10 @@ import View from "./View";
 import icons from "url:../../assets/icons.svg";
 import { Fraction } from "../../../node_modules/fractional";
 
-class RecipeView {
+class RecipeView extends View {
   _parentEl = document.querySelector(".recipe");
-  _data;
   _errorMessage = `We could not find that recipe, Please try another one!`;
   _message = `Start by searching for a recipe or an ingredient. Have fun!`;
-
-  render(data) {
-    this._data = data;
-    const markup = this._generaterMarkup();
-    this._clear();
-    this._parentEl.insertAdjacentHTML("afterbegin", markup);
-  }
-
-  _clear() {
-    this._parentEl.innerHTML = "";
-  }
-
-  renderSpinner = function () {
-    const markup = `<div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-   </div>`;
-    this._clear();
-    this._parentEl.insertAdjacentHTML("afterbegin", markup);
-  };
-
-  renderError(message = this._errorMessage) {
-    const markup = ` 
-    <div class="error">
-    <div>
-      <svg>
-        <use href="${icons}#icon-alert-triangle"></use>
-      </svg>
-    </div>
-    <p>${message}</p>
-  </div>`;
-
-    this._clear();
-    this._parentEl.insertAdjacentHTML("afterbegin", markup);
-  }
-
-  renderMessage(message = this._message) {
-    const markup = `  
-    <div class="message">
-    <div>
-      <svg>
-        <use href="${icons}#icon-smile"></use>
-      </svg>
-    </div>
-    <p>${message}</p>
-  </div> `;
-
-    this._clear();
-    this._parentEl.insertAdjacentHTML("afterbegin", markup);
-  }
 
   addHandlerRender(handler) {
     ["hashchange", "load"].forEach((ev) =>
