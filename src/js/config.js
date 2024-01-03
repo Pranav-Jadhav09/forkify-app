95075fbe13778ddfd914e00bf912d6476f867554
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-03 11:20:51 +0530

    code: 📝 moved API to config

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+/**
+ * Put all the variables that should be constant and should be reused accross the project and goal along with tese variables is that it will allow us to easily configure our project by simply changing some of the data that is here in config file
+ * */
+
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const API_KEY = "";
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
 import { async } from "regenerator-runtime";
+import { API_URL, API_KEY } from "./config";
 
 export const state = {
   recipe: {},
@@ -6,9 +7,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
+    const res = await fetch(`${API_URL}/${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} ${res.status}`);
