d86d8f430caa6fac60a37da8a2b4e73fa0c4ee6a
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-03 12:26:12 +0530

    refact: 🔄 getJSON from helper

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,3 @@
-/**
- * Put all the variables that should be constant and should be reused accross the project and goal along with tese variables is that it will allow us to easily configure our project by simply changing some of the data that is here in config file
- * */
-
+export const TIMEOUT_SEC = 10;
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
-export const API_KEY = "";
+export const API_KEY = ``;
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,6 @@
 import { async } from "regenerator-runtime";
-import { API_URL, API_KEY } from "./config";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helper.js";
 
 export const state = {
   recipe: {},
@@ -7,10 +8,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(`${API_URL}/${id}`);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -26,6 +24,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 🤯`);
   }
 };
